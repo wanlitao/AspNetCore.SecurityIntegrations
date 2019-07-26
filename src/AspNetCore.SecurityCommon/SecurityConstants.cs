9b231ca20dc9371520f107888c36cdbb6b25cd3d
万里涛 <wanlitao_3055601@outlook.com>
2019-07-26 17:38:13 +0800

    fix calc timestamp bug

--- src/AspNetCore.SecurityCommon/SecurityConstants.cs
+++ src/AspNetCore.SecurityCommon/SecurityConstants.cs
@@ -9,6 +9,6 @@ namespace AspNetCore.SecurityCommon
         public const string HttpHeaders_SignatureKey = "X-SSL-SignatureKey";
         public const string HttpHeaders_Signature = "X-SSL-Signature";
 
-        public static DateTime Timestamp_StartUtcDateTime = new DateTime(1970, 1, 1).ToUniversalTime();
+        public static DateTime Timestamp_StartUtcDateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
     }
 }
